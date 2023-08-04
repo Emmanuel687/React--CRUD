966a74c63abcce1a31605a1bca0e1086d78c3222
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-08-04 16:01:39 +0300

    Dev:-Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from './Form';
+import ItemList from './ItemList';
+
+const App = () => {
+  return (
+    <div>
+      <Form />
+      <ItemList />
+    </div>
+  )
+}
+
+export default App
